b84b217b7b28ef5e3e21afa07e4fce2f875c2e94
mohan9638 <158275364+mohan9638@users.noreply.github.com>
2025-05-24 22:16:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-MONGO_URI = environ.get("MONGO_URI", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
+API_ID = int(environ.get("25693368", ""))
+API_HASH = environ.get("2dcf91b0f99c0b9d4875e87020e6bd07", "")
+BOT_TOKEN = environ.get("8000283060:AAF20ajaxXZGVKEpwvuSygGx351OF4nvA8s", "")
+MONGO_URI = environ.get("mongodb+srv://pawedal810:HOFEIUUSvhAWHHW7@cluster0.8sxxbgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "")
+OWNER_ID = int(environ.get("5927517339", ""))
